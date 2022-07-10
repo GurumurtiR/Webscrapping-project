8018bafb2f9364baf8cb18db0388a46e74fb1074
Gurumurti Rudraganti <108205120+GurumurtiR@users.noreply.github.com>
2022-07-10 12:26:17 +0530

    Add files via upload

---
+++ STUDENT  MARKS AND GRADE.py
@@ -0,0 +1,16 @@
+# STUDENT OBTAINED MARKS AND GRADE
+
+marks_obtained = 35
+if marks_obtained >= 85:
+    print("Congrats, you have scored Distinction")
+    print("All the best for your future")
+
+elif 60 <= marks_obtained < 85:
+    print("Congrats, you have scored First Class")
+
+elif 35 <= marks_obtained < 60:
+    print("Congrats, you have passed the exam")
+
+else:
+    print("Sorry, you have failed the exam,better luck next time")
+print("**********RESULT OF THE EXAM**********")
